464a2a7735ee2791a7e0f82e256728a193db7182
Moritz Huber <2nd@moritzhuber.de>
2026-09-23 00:08:27 +0200

    particle on break

--- src/main/java/tk/alex3025/headstones/utils/Headstone.java
+++ src/main/java/tk/alex3025/headstones/utils/Headstone.java
@@ -111,7 +111,7 @@ public class Headstone {
 
         event.setDropItems(Headstones.getInstance().getConfig().getBoolean("drop-player-head"));
 
-        player.spawnParticle(Particle.DUST, this.location.add(0.5, 0.2, 0.5), 10, 0.2, 0.1, 0.2, new Particle.DustOptions(Color.fromRGB(255, 255, 255), 1.5F));
+        player.spawnParticle(Particle.DUST, this.location.clone().add(0.5, 0.2, 0.5), 10, 0.2, 0.1, 0.2, new Particle.DustOptions(Color.WHITE, 1.5F));
         player.playSound(this.location, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 2.0F, 1.0F);
 
         new Message(player).translation("headstone-broken").prefixed(false).send();
